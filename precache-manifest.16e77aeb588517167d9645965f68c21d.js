cfc690c4838efbee1d02314ef0a39441f5179a9c
minhtamos <minhtamos@minhtamoss-Mac-mini.local>
2020-01-22 21:55:59 +0700

    Updates

---
+++ precache-manifest.16e77aeb588517167d9645965f68c21d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "248527f4c0113bf534e67ee09a978113",
+    "url": "/2048_REACT/index.html"
+  },
+  {
+    "revision": "859384f6dd838726d6bf",
+    "url": "/2048_REACT/static/css/main.b9c1ac3d.chunk.css"
+  },
+  {
+    "revision": "9ff01078ca94e8879b8e",
+    "url": "/2048_REACT/static/js/2.e819c692.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/2048_REACT/static/js/2.e819c692.chunk.js.LICENSE"
+  },
+  {
+    "revision": "859384f6dd838726d6bf",
+    "url": "/2048_REACT/static/js/main.a6c988d7.chunk.js"
+  },
+  {
+    "revision": "83d7edb99fc943d64a7e",
+    "url": "/2048_REACT/static/js/runtime-main.9ffa8280.js"
+  }
+]);
