b04a5a0690e596902f456adcac14d019d51a2799
minhtamos <minhtamos@minhtamoss-Mac-mini.local>
2020-01-24 00:38:45 +0700

    Updates

--- precache-manifest.4e56ae7aaee1d366faeeabc016c45fa5.js
+++ precache-manifest.4e56ae7aaee1d366faeeabc016c45fa5.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "462834b8b9823bb94f5fba6076768405",
+    "revision": "2653fb8dd4ad690837a2e8ffcee6dcff",
     "url": "/2048_REACT/index.html"
   },
   {
-    "revision": "8f5809911e37f1a59782",
+    "revision": "a6f9d40a48c0b99cbd9f",
     "url": "/2048_REACT/static/css/main.e6f924c1.chunk.css"
   },
   {
-    "revision": "2f30e10ec464ac2f2208",
-    "url": "/2048_REACT/static/js/2.81bb982b.chunk.js"
+    "revision": "899c5c662214a31cdbf9",
+    "url": "/2048_REACT/static/js/2.13d9fa4a.chunk.js"
   },
   {
     "revision": "e928fe768baa9832b5bc57eae021f30c",
-    "url": "/2048_REACT/static/js/2.81bb982b.chunk.js.LICENSE"
+    "url": "/2048_REACT/static/js/2.13d9fa4a.chunk.js.LICENSE"
   },
   {
-    "revision": "8f5809911e37f1a59782",
-    "url": "/2048_REACT/static/js/main.ac468693.chunk.js"
+    "revision": "a6f9d40a48c0b99cbd9f",
+    "url": "/2048_REACT/static/js/main.5d41f097.chunk.js"
   },
   {
     "revision": "2943133b6ee80bd69460",
