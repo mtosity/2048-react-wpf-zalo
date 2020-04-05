64df34c7d0e1644f113c80493bf59138937aa377
minhtamos <tam310599@gmail.com>
2020-04-05 08:42:15 +0700

    increase animation, reset only when arrow key

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -13,7 +13,7 @@ const Box: React.FC<BoxProps> = ({ box }: BoxProps) => {
   const {title, color, titleColor} = convertTitleToBoxInfo(box.title)
 
   const [state, toggle] = useState(true)
-  const { x } = useSpring({ from: { x: 0 }, x: state ? 1 : 0, config: { duration: 200 } })
+  const { x } = useSpring({ from: { x: 1 }, x: state ? 1 : 0, config: { duration: 200 } })
 
 
   useEffect(() => {
@@ -30,7 +30,7 @@ const Box: React.FC<BoxProps> = ({ box }: BoxProps) => {
           // range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
           // output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1]
           range: [0, 0.5, 1],
-          output: [1, 1.1, 1]
+          output: [1, 1.25, 1]
         })
         .interpolate(x => `scale(${x})`),
 
